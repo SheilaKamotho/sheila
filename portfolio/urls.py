41a3c215c9b0bacb344b4ea84f324c57734b0f93
Kamotho Sheila <kamothosheila@gmail.com>
2020-11-16 17:53:50 +0300

    add changes

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.home,name = 'home'),
+    url('^contact$',views.contact,name = 'contact'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
